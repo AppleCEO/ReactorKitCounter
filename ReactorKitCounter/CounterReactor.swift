2b74593edb2f364cd4f596957632245a24166af3
도미닉 <kiljh@me.com>
2025-07-23 20:59:41 +0900

    Mutate, Reduce - 동작 구현

--- ReactorKitCounter/CounterReactor.swift
+++ ReactorKitCounter/CounterReactor.swift
@@ -25,10 +25,22 @@ final class CounterReactor: Reactor {
     var initialState: State = State()
     
     func mutate(action: Action) -> Observable<Mutation> {
-        
+        switch action {
+        case .plusButtonTapped:
+            return .just(.increaseCount)
+        case .minusButtonTapped:
+            return .just(.decreaseCount)
+        }
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
-        
+        var newState = state
+        switch mutation {
+        case .increaseCount:
+            newState.count += 1
+        case .decreaseCount:
+            newState.count -= 1
+        }
+        return newState
     }
 }
