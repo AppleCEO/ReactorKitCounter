3491486923cf5161ad634933bc7bc9c000ba40a0
도미닉 <kiljh@me.com>
2025-07-23 21:12:59 +0900

    UI 코드 베이스로 구성

--- ReactorKitCounter/CounterViewController.swift
+++ ReactorKitCounter/CounterViewController.swift
@@ -10,35 +10,76 @@ import ReactorKit
 import RxCocoa
 
 class CounterViewController: UIViewController, View {
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
+    let stackView = UIStackView()
+    let countLabel = UILabel()
+    let minusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("-", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    let plusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("+", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     var disposeBag: RxSwift.DisposeBag = DisposeBag()
 
     func bind(reactor: CounterReactor) {
-        plusButton.rx.tap
-            .map { Reactor.Action.plusButtonTapped }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-        
-        minusButton.rx.tap
-            .map { Reactor.Action.minusButtonTapped }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-        
-        reactor.state.map { $0.count }
-            .distinctUntilChanged()
-            .bind(with: self) { [weak self] owner, count in
-                self?.label.text = String(count)
-            }
-            .disposed(by: disposeBag)
+        setPlusButtonTap(reactor: reactor)
+        setMinusButtonTap(reactor: reactor)
+        bindCountState(reactor: reactor)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureView()
     }
 
-
+    private func configureView() {
+        addSubViews()
+        setAutolayout()
+    }
+    
+    private func addSubViews() {
+        view.backgroundColor = .white
+        view.addSubview(stackView)
+        [minusButton, countLabel, plusButton].forEach { view in
+            stackView.addArrangedSubview(view)
+        }
+    }
+    
+    private func setAutolayout() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func setPlusButtonTap(reactor: CounterReactor) {
+        plusButton.rx.tap
+            .map { Reactor.Action.plusButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    private func setMinusButtonTap(reactor: CounterReactor) {
+        minusButton.rx.tap
+            .map { Reactor.Action.minusButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindCountState(reactor: CounterReactor) {
+        reactor.state.map { $0.count }
+            .distinctUntilChanged()
+            .bind(with: self) { [weak self] owner, count in
+                self?.countLabel.text = String(count)
+            }
+            .disposed(by: disposeBag)
+    }
 }
 
