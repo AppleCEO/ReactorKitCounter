a56a51e037a6a6259bb95cdd899b49b2de6f786c
도미닉 <kiljh@me.com>
2025-07-23 20:54:33 +0900

    버튼 탭 시 리엑터킷의 액션 실행

--- ReactorKitCounter/CounterViewController.swift
+++ ReactorKitCounter/CounterViewController.swift
@@ -16,6 +16,15 @@ class CounterViewController: UIViewController, View {
     var disposeBag: RxSwift.DisposeBag = DisposeBag()
 
     func bind(reactor: CounterReactor) {
+        plusButton.rx.tap
+            .map { Reactor.Action.plusButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        minusButton.rx.tap
+            .map { Reactor.Action.minusButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
     }
     
